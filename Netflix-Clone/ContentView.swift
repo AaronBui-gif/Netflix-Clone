9667637dfedbaadc48dea1ae9f4113935b672e60
AaronBui-gif <s3740934@rmit.edu.vn>
2022-08-29 19:06:28 +0700

    Initial Commit

---
+++ Netflix-Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Netflix-Clone
+//
+//  Created by Huy Bui Thanh on 29/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Netflix-Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix-Clone
+//
+//  Created by Huy Bui Thanh on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
